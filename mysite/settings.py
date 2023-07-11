6b436c90ff5109335d071d1b2f7f0b15a5565336
Korossol <zoukway@gmail.com>
2023-07-11 15:36:49 +0100

    11072023 v2 setting.py  change  for makemigrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# to avoid this warning in python anywhere when making migration:
+#       (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField'.
+        #HINT: Configure the DEFAULT_AUTO_FIELD setting or the PollConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'.
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
